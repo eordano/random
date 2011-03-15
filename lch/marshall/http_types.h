e3778d90d75ab567e6a2e6fdb7e3be80371aaf69
Esteban Ordano <eordano@gmail.com>
2011-03-15 18:10:35 -0300

    Marshalling 'listo', falta debuggear

--- lch/marshall/http_types.h
+++ lch/marshall/http_types.h
@@ -4,10 +4,42 @@
 #include "../util/dlinked_list.h"
 #include "../util/defines.h"
 
+typedef enum {
+    OPTIONS, GET, HEAD, POST, PUT,  DELETE, TRACE, CONNECT
+} request_type_t;
+typedef int status_code_t;
+
+#define SC_OK 200
+#define SC_BAD_REQUEST 400
+#define SC_NOT_FOUND 404
+#define SC_SERVER_ERROR 500
+
+struct pair_strings{
+    char* key;
+    char* value;
+};
+
 struct http_request_t {
+    request_type_t request_type;
+    char* path;
+
+    // Privileged headers
+    char* host;
+    char* user_agent;
+    char* referer;
+
+    struct dlinked_list* headers;
+    char* data;
 };
 
 struct http_response_t {
+    status_code_t status_code;
+
+    char* content_type;
+    size_t content_length;
+    struct dlinked_list footers;
+
+    char* content;
 };
 
 #endif // __http_types_h__
--- lch/marshall/request_marshall.h
+++ lch/marshall/request_marshall.h
@@ -9,5 +9,8 @@
 struct http_request_t* request_marshall(char* raw_http_request);
 char* request_unmarshall(struct http_response_t* response);
 
+void free_http_response(struct http_response_t* response);
+void free_http_request(struct http_request_t* request);
+
 #endif // __request_marshall_h__
 
