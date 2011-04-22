fbcfdea8f38f1f47274ef0bb7885fc6bafba6f2e
Esteban Ordano <eordano@gmail.com>
2011-04-22 14:35:24 -0300

    removed lch from here

--- lch/include/lch.h
+++
@@ -1,9 +0,0 @@
-#ifndef __request_marshall_h__
-#define __request_marshall_h__
-
-#include "http_types.h"
-
-void init_lch(int port, struct http_response_t* (*callback)(struct http_request_t* req));
-
-#endif // __request_marshall_h__
-
